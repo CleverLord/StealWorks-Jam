db5f0e36c4ee3af89cdbca7ab56521cb59f49c42
Krzysztof Łazarz <kjlazarz@student.agh.edu.pl>
2022-10-29 17:42:14 +0200

    Orbiter

---
+++ StealWorks GameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class GameManager : MonoBehaviour
+{
+    public Color fogColor = Color.yellow;
+    [Range(0.005f,0.04f)]
+    public float fogDencity = 0.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.fogColor = fogColor;
+        Camera.main.backgroundColor = fogColor;
+        RenderSettings.fogDensity = fogDencity;
+    }
+}
---
+++ StealWorks GameJam/Assets/Scripts/Player/OrbitingBall.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitingBall : MonoBehaviour
+{
+    public float orbitingRadius = 0.7f;
+    public float orbitingHeight = 1f;
+    public GameObject orbiter;
+    public GameObject player;
+    public float closeDistance = 0.1f;
+    public float angualarSpeed = 12f;
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!orbiter)
+        {
+            orbiter = new GameObject("orbiter");
+            orbiter.transform.parent = player.transform;
+            Vector3 dir = this.transform.position - player.transform.position;
+            orbiter.transform.position = dir.normalized * orbitingRadius+ player.transform.position;
+            orbiter.transform.position = new Vector3(orbiter.transform.position.x, orbitingHeight, orbiter.transform.position.z);
+        }
+    }
+
+    public bool isCloseToOrbit => (this.transform.position - orbiter.transform.position).magnitude < closeDistance;
+    public void Update()
+    {
+        if (isCloseToOrbit)
+            orbiter.transform.RotateAround(player.transform.position, Vector3.up, angualarSpeed * Time.deltaTime);
+        MoveTowardsOrbit(); 
+    }
+    public float maxSpeed = 1;
+    public void MoveTowardsOrbit()
+    {
+        Vector3 dir = orbiter.transform.position - transform.position;
+        if (dir.magnitude > maxSpeed * Time.deltaTime)
+            dir = dir.normalized * maxSpeed * Time.deltaTime;
+        transform.position += dir;
+    }
+
+}
