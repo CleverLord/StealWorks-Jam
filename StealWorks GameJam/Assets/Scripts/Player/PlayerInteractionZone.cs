a4fcc8d9821d23c444b2e8cabaf8848d1a2f2d63
Krzysztof Łazarz <kjlazarz@student.agh.edu.pl>
2022-10-29 22:17:30 +0200

    Ui and interactions

--- StealWorks GameJam/Assets/Scripts/Player/PlayerInteractionZone.cs
+++ StealWorks GameJam/Assets/Scripts/Player/PlayerInteractionZone.cs
@@ -6,24 +6,38 @@ using UnityEngine.Events;
 public class PlayerInteractionZone : MonoBehaviour
 {
     public bool activated = false;
-    public UnityEvent onEntered;
+    public float interactionTime = 3;
+    public UnityEvent onInteracted;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void TestInteract()
+    {
+        Debug.Log("Interacted!");
     }
     private void OnTriggerEnter(Collider other)
     {
+        if (activated) return;
         if (other.CompareTag("Player"))
         {
-
+            UiFiller.uf.StartSliding(interactionTime, () => { onInteracted.Invoke(); this.activated = true; });
         }
-        
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (activated) return;
+        if (other.CompareTag("Player"))
+        {
+            UiFiller.uf.StopSliding();
+        }
     }
 }
--- StealWorks GameJam/Assets/Scripts/Player/UiFiller.cs
+++ StealWorks GameJam/Assets/Scripts/Player/UiFiller.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class UiFiller : MonoBehaviour
 {
+    public Slider s;
+    public float tStart;
+    public float tEnd;
+    public bool sliding = true;
+    public static UiFiller uf;
+    public Action onEnd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uf = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!sliding)
+            return;
+        s.value = Mathf.InverseLerp(tStart, tEnd, Time.time);
+        if (s.value == 1)
+        {
+            onEnd?.Invoke();
+            StopSliding();
+        }
+    }
+
+    public void StartSliding(float slidingTime,Action a)
+    {
+        onEnd = a;
+        tStart = Time.time;
+        tEnd = Time.time + slidingTime;
+        s.gameObject.SetActive(true);
+        sliding = true;
+    }
+    public void StopSliding()
+    {
+        s.gameObject.SetActive(false);
+        sliding = false;
     }
 }
