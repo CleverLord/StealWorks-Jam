72dc942c75309a39b650ef1f542c05ece9b344b0
Vedemin <VedeminFF@gmail.com>
2022-10-30 16:32:36 +0100

    Fog and grass

--- StealWorks GameJam/Assets/Scripts/Player/PlayerMovement.cs
+++ StealWorks GameJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,10 +9,13 @@ public class PlayerMovement : MonoBehaviour
     public float mouseSpeedX = 1;
     public float mouseSpeedY = 1;
     public bool invertY;
+    private float gSpeed = 0f;
     CharacterController cc;
     public GameObject cameraParent;
     Vector3 lookVector = new Vector3(0,0,0);
-    // Start is called before the first frame update
+    public static float lifeTime = 180;
+    public UnityAction onDeath;
+
     void Start()
     {
         cc = GetComponent<CharacterController>();
@@ -19,20 +23,23 @@ public class PlayerMovement : MonoBehaviour
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         UpdatePosition();
-        //transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * mouseSpeedX);
-        //cameraPivot.transform.Rotate(Vector3.right, Input.GetAxis("Mouse Y") * mouseSpeedY * (invertY ? 1:-1));
     }
     void UpdatePosition()
     {
+        if (!cc.isGrounded)
+        {
+            gSpeed += Physics.gravity.y * Time.deltaTime;
+        } else
+        {
+            gSpeed = 0;
+        }
         Vector3 v3In = cameraParent.transform.right* Input.GetAxis("Horizontal") + cameraParent.transform.forward * Input.GetAxis("Vertical");
         if (v3In.magnitude > 0.01)
             lookVector = v3In;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookVector.normalized, Vector3.up), 0.1f);
-        //transform.Translate(v3In * speed*Time.deltaTime);
-        cc.SimpleMove(v3In * speed);
+        cc.Move((Vector3.ClampMagnitude(v3In, 1) * speed - Vector3.down * gSpeed) * Time.deltaTime);
     }
 }
