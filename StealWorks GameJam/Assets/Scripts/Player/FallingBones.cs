6efdc47c5728dbd22aa5aac6205ee7deeb2b0168
Krzysztof Łazarz <kjlazarz@student.agh.edu.pl>
2022-10-30 12:08:10 +0100

    FallingBones

---
+++ StealWorks GameJam/Assets/Scripts/Player/FallingBones.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FallingBones : MonoBehaviour
+{
+    List<Rigidbody> boneObjects;
+    public float time = 180;
+    int lastIdx = 0;
+    void Start()
+    {
+        //disable all colliders ffs
+        //this.GetComponentsInChildren<Collider>().ToList().ForEach(c => c.isTrigger = true);
+
+        //get only rigidbodies with meshes
+        boneObjects = this.GetComponentsInChildren<Rigidbody>().ToList()
+            .Where(r => r.GetComponentInChildren<MeshFilter>())
+            .OrderBy(r => r.gameObject.transform.position.y).ToList();
+        Debug.Log(boneObjects.Count+" bones");
+
+    }
+    private void Update()
+    {
+        float f = Mathf.InverseLerp(0, time, Time.time);
+        for (int i = lastIdx; i <= f * boneObjects.Count && i < boneObjects.Count; i++)
+        {
+            boneObjects[i].isKinematic = false;
+            boneObjects[i].useGravity = true;
+            boneObjects[i].transform.parent = null;
+            //boneObjects[i].GetComponentInChildren<MeshCollider>().isTrigger = false;
+            //var sc= boneObjects[i].gameObject.AddComponent<SphereCollider>();
+            //sc.radius = 0.01f;
+            lastIdx = i;
+        }
+    }
+}
